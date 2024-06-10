202aa5f6b76d4d0fbda5d0e4999893ca2cad12d3
t0vers <Kondratiev.Ilya@urfu.me>
2024-06-10 18:36:57 +0500

    added auth + register with fastapi-users

---
+++ user-service/database/database.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTable
+from sqlalchemy import Column, Boolean, String, Integer, TIMESTAMP, ForeignKey
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from models.user import Role
+
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:1q2w3e4r5t!Q@postgres:5432/hotels"
+Base = declarative_base()
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column("id", Integer, primary_key=True)
+    email = Column("email", String, nullable=False)
+    username = Column("username", String, nullable=False)
+    registered_at = Column("registered_at", TIMESTAMP, default=datetime.now())
+    role_id = Column("role_id", Integer, ForeignKey(Role.c.id))
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=True, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
--- user-service/models/user.py
+++ user-service/models/user.py
@@ -1,25 +1,28 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, JSON
+from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, JSON, Boolean
 from sqlalchemy.testing.schema import Column
 
 metadata = MetaData()
 
-roles = Table(
-    "roles",
+Role = Table(
+    "role",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("permissions", JSON)
 )
 
-users = Table(
-    "users",
+User = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
+    Column("hashed_password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.now()),
-    Column("role_id", Integer, ForeignKey("roles.id"))
+    Column("role_id", Integer, ForeignKey(Role.c.id)),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=True, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False)
 )
