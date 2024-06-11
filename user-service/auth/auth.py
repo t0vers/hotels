dcefaa888958caa3a00034d10efedf1625ba8923
t0vers <Kondratiev.Ilya@urfu.me>
2024-06-11 13:38:42 +0500

    switch cookie to bearer

--- user-service/auth/auth.py
+++ user-service/auth/auth.py
@@ -1,8 +1,8 @@
-from fastapi_users.authentication import AuthenticationBackend, JWTStrategy, CookieTransport
+from fastapi_users.authentication import AuthenticationBackend, JWTStrategy, BearerTransport
 
 SECRET = "SECRET"
 
-cookie_transport = CookieTransport(cookie_name="hotels", cookie_max_age=3600)
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 
 def get_jwt_strategy() -> JWTStrategy:
@@ -11,6 +11,6 @@ def get_jwt_strategy() -> JWTStrategy:
 
 auth_backend = AuthenticationBackend(
     name="jwt",
-    transport=cookie_transport,
+    transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
